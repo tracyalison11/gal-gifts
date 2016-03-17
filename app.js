37d6f185e8e1d608ea46def202885810a91b34e8
tracyalison11 <tracyak13@gmail.com>
2016-03-16 22:05:56 -0700

    First prototype

---
+++ app.js
@@ -0,0 +1,20 @@
+//$(document).ready(function(){
+//  $(".gift-categories").hide();
+//});
+
+var menuControl = $(".girl-menu");
+var giftMenu = $(".gift-categories");
+
+
+//$(".search-menu span").on("click", function(){
+//
+//    if(menuControl.hasClass('fa-caret-right')) {
+//      menuControl.removeClass('fa-caret-right').addClass('fa-carent-down');
+//      giftMenu.slideDown(1000);
+//    } else {
+//      menuControl.removeClass('fa-carent-down').addClass('fa-caret-right');
+//      giftMenu.slideUp(1000);
+//
+//    }
+//
+//});
